ef57909e48f54827e5447861f2e1688f84a698e6
Alex Bikfalvi <alex@bikfalvi.com>
2013-01-23 19:52:57 +0100

    Creates a new image list box control.

---
+++ DotNetApi/Windows/Controls/ImageListBox.cs
@@ -0,0 +1,134 @@
+﻿/* 
+ * Copyright (C) 2012-2013 Alex Bikfalvi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DotNetApi.Windows.Controls
+{
+	public delegate void ImageListBoxItemActivateEventHandler(object sender, ImageListBoxItem item);
+
+	/// <summary>
+	/// An image list box contro.
+	/// </summary>
+	public class ImageListBox : ListBox
+	{
+		private int imageWidth = 64;
+
+		/// <summary>
+		/// Creates a new image list box instance.
+		/// </summary>
+		public ImageListBox()
+		{
+			this.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+			this.ItemHeight = 48;
+
+			this.DrawItem += OnDrawItem;
+			this.DoubleClick += OnDoubleClick;
+		}
+
+		/// <summary>
+		/// Gets or sets the image width.
+		/// </summary>
+		public int ImageWidth
+		{
+			get { return this.imageWidth; }
+			set { this.imageWidth = value; this.Invalidate(); }
+		}
+
+		/// <summary>
+		/// An event raised when the user activates an item.
+		/// </summary>
+		public event ImageListBoxItemActivateEventHandler ItemActivate;
+
+		/// <summary>
+		/// Adds a new item to the image list box.
+		/// </summary>
+		/// <param name="text">The item text.</param>
+		/// <param name="image">The item image.</param>
+		public void AddItem(string text, Image image)
+		{
+			// Create a new image list box item.
+			ImageListBoxItem item = new ImageListBoxItem(text, image);
+
+			// Add the item to the list box.
+			this.Items.Add(item);
+		}
+
+		/// <summary>
+		/// Draws the image list box item.
+		/// </summary>
+		/// <param name="sender">The sender object.</param>
+		/// <param name="e">The event arguments.</param>
+		void OnDrawItem(object sender, DrawItemEventArgs e)
+		{
+			if ((e.Index < 0) || (e.Index >= this.Items.Count)) return;
+
+			// Get the image list box item.
+			ImageListBoxItem item = this.Items[e.Index] as ImageListBoxItem;
+
+			Rectangle rectText = new Rectangle(
+				e.Bounds.Left + this.ImageWidth + 5,
+				e.Bounds.Top,
+				e.Bounds.Width - this.ImageWidth - 5,
+				e.Bounds.Height);
+
+			Rectangle rectImage = new Rectangle(
+				e.Bounds.Left,
+				e.Bounds.Top,
+				this.imageWidth,
+				e.Bounds.Height);
+
+			// Draw a background rectangle.
+			e.Graphics.FillRectangle(new SolidBrush(e.BackColor), e.Bounds);
+
+			// Draw the text
+			if (null != item.Text)
+			{
+				TextRenderer.DrawText(
+					e.Graphics,
+					item.Text,
+					this.Font,//new System.Drawing.Font(SystemFonts.MenuFont.FontFamily, this.Font., FontStyle.Bold),
+					rectText,
+					e.ForeColor,
+					TextFormatFlags.EndEllipsis | TextFormatFlags.Left | TextFormatFlags.VerticalCenter);
+			}
+
+			// Draw the image.
+			if (null != item.Image)
+			{
+				e.Graphics.DrawImage(item.Image, rectImage);
+			}
+		}
+
+		/// <summary>
+		/// An event handler called when the user double click on an item.
+		/// </summary>
+		/// <param name="sender">The sender object.</param>
+		/// <param name="e">The event arguments.</param>
+		void OnDoubleClick(object sender, EventArgs e)
+		{
+			if ((this.SelectedItem != null) && (this.ItemActivate != null))
+				this.ItemActivate(sender, this.SelectedItem as ImageListBoxItem);
+		}
+	}
+}
---
+++ DotNetApi/Windows/Controls/ImageListBoxItem.cs
@@ -0,0 +1,65 @@
+﻿/* 
+ * Copyright (C) 2012-2013 Alex Bikfalvi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace DotNetApi.Windows.Controls
+{
+	/// <summary>
+	/// An image list box item.
+	/// </summary>
+	[Serializable]
+	public class ImageListBoxItem
+	{
+		private string text;
+		private Image image;
+
+		/// <summary>
+		/// Creates a new image list box item.
+		/// </summary>
+		/// <param name="text">The item text.</param>
+		/// <param name="image">The item image.</param>
+		public ImageListBoxItem(string text, Image image)
+		{
+			this.text = text;
+			this.image = image;
+		}
+
+		/// <summary>
+		/// Gets or sets the item text.
+		/// </summary>
+		public string Text
+		{
+			get { return this.text; }
+			set { this.text = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the item image.
+		/// </summary>
+		public Image Image
+		{
+			get { return this.image; }
+			set { this.image = value; }
+		}
+	}
+}
