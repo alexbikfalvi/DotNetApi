56dd8ad6908711d655b30dabc3762b27501b72f5
Alex Bikfalvi <alex@bikfalvi.com>
2013-11-29 17:18:55 +0100

    Added a statistics list box and theme tab control.

---
+++ DotNetApi/Windows/Controls/StatisticsItem.cs
@@ -0,0 +1,63 @@
+﻿/* 
+ * Copyright (C) 2012-2013 Alex Bikfalvi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+using System;
+using System.ComponentModel;
+
+namespace DotNetApi.Windows.Controls
+{
+	/// <summary>
+	/// A statistics list box item.
+	/// </summary>
+	[DesignTimeVisible(false)]
+	public sealed class StatisticsItem : Component
+	{
+		private string text;
+
+		/// <summary>
+		/// Creates a new statistics item instance.
+		/// </summary>
+		public StatisticsItem()
+		{
+		}
+
+		/// <summary>
+		/// Creates a new statistics item with the specified text.
+		/// </summary>
+		/// <param name="text">The item text</param>
+		public StatisticsItem(string text)
+		{
+			this.text = text;
+		}
+
+		// Public properties.
+
+		/// <summary>
+		/// Gets or sets the item text.
+		/// </summary>
+		public string Text
+		{
+			get { return this.text; }
+			//set { this.OnTextSet(value); }
+		}
+
+		// Private methods.
+
+		//public 
+	}
+}
---
+++ DotNetApi/Windows/Controls/ThemeTabControl.cs
@@ -0,0 +1,60 @@
+﻿/* 
+ * Copyright (C) 2013 Alex Bikfalvi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DotNetApi.Windows.Controls
+{
+	/// <summary>
+	/// A theme tab control.
+	/// </summary>
+	public sealed class ThemeTabControl : TabControl
+	{
+		public ThemeTabControl()
+		{
+			this.DrawMode = TabDrawMode.OwnerDrawFixed;
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			using (SolidBrush brush = new SolidBrush(Color.Yellow))
+			{
+				e.Graphics.FillRectangle(brush, this.Bounds);
+			}
+		}
+
+		protected override void OnPaintBackground(PaintEventArgs e)
+		{
+			using (SolidBrush brush = new SolidBrush(Color.Blue))
+			{
+				e.Graphics.FillRectangle(brush, this.Bounds);
+			}
+		}
+
+		protected override void OnDrawItem(DrawItemEventArgs e)
+		{
+			//base.OnDrawItem(e);
+			using (SolidBrush brush = new SolidBrush(Color.Black))
+			{
+				e.Graphics.FillRectangle(brush, e.Bounds);
+			}
+		}
+	}
+}
